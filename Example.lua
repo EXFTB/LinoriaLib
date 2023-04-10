98dfd0b1d6aeae4d84bd6951cb8f6f0e0d15c06f
EXTb <100034435+EXFTB@users.noreply.github.com>
2023-04-10 12:53:29 +0200

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -8,6 +8,7 @@ local Repostry = "https://raw.githubusercontent.com/EXFTB/LinoriaLib/main/"
 local Library = loadstring(game:HttpGet(Repostry .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(Repostry .. "ThemeManager.lua"))()
 local SaveManager = loadstring(game:HttpGet(Repostry .. "SaveManager.lua"))()
+local ServiceManager = loadstring(game:HttpGet(Repostry.. "ServiceManager.lua"))
 --\\----//--
 
 --//----\\--
@@ -22,12 +23,15 @@ local Watermark = Library:SetWatermark("Script | Version | Game")
 
 local Tabs = {
     ["Main"] = Window:AddTab("Main"),
+    ["UI-Settings"] = Window:AddTab("UI-Settings"),
 }
 --\\----//--
 
 --//----\\--
 local TestingBox = Tabs["Main"]:AddLeftGroupbox("Testing")
 local TestingBox2 = Tabs["Main"]:AddRightGroupbox("Testing2")
+
+local SettingBox = Tabs["UI-Settings"]:AddLeftGroupbox("Settings")
 --\\----//--
 
 --//----\\--
@@ -86,7 +90,22 @@ TestingBox2:AddToggle("Toggle2", {
     NoUI = false
 })
 
-TestingBox2:AddLabel("Toggle UI"):AddKeyPicker("ToggleUIKey", {
+TestingBox2:AddSlider("HitChance", {
+    Text = "Hitchance",
+    Default = 0,
+    Min = 0,
+    Max = 100,
+    Rounding = 1,
+    Compact = false,
+    Callback = function(Value)
+        print("Hitchance: ", Value)
+    end
+})
+--\\----//--
+
+--//----\\--
+SaveManager:BuildConfigSection(Tabs['UI-Settings'])
+SettingBox:AddLabel("Toggle UI"):AddKeyPicker("ToggleUIKey", {
     Mode = "Toggle",
     Default = "RightShift",
     Text = "",
@@ -95,4 +114,5 @@ TestingBox2:AddLabel("Toggle UI"):AddKeyPicker("ToggleUIKey", {
         Library.ToggleKeybind = Value
     end
 })
+ThemeManager:ApplyToGroupbox(SettingBox)
 --\\----//--
